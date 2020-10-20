769f0f210ccbb0bdb7babb53929fa13b7d0bf95a
Pedro Alves <alvesouza.pedro97@gmail.com>
2020-10-20 09:03:43 -0300

    envio de email

--- routes/Api/user/user.js
+++ routes/Api/user/user.js
@@ -1,6 +1,14 @@
 const express = require('express');
+// const mailgun = require("mailgun-js");
+var nodemailer = require('nodemailer');
+// const DOMAIN = 'financemanager.com';
+// const DOMAIN = 'sandbox8397a09e1ae84939aa438d7cd140d261.mailgun.org';
+const DOMAIN = 'gmail.com';
+// const DOMAIN = 'gmail';
+// const APIKEY = '3703153f6bb3038c696a95edb45e07b3-53c13666-cc8eebf0';
 const router = express.Router();
-
+const PASS = "byt6%&*uig9HKhuf6DDCctfj9";
+const mail = "managerfinance.ces26@gmail.com";
 router.post('/', (req, res) => {
     var body = {};
     console.log(req);
@@ -8,6 +16,29 @@ router.post('/', (req, res) => {
     body.name = req.body.name;
     body.email = req.body.email;
     console.log(body);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: mail,
+            pass: PASS
+        }
+    });
+
+    var mailOptions = {
+        from: mail,
+        to: body.email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
     res.send(body);
 });
 
