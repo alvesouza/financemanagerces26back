122fa6017db8fb894b9ef5ef5c8064293719c2c8
Pedro Alves <alvesouza.pedro97@gmail.com>
2020-10-21 23:56:19 -0300

    cors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const cors = require('cors');
 
 // app.use(allowCrossDomain);
 // view engine setup
+app.options('*', cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
