bdb84273f32129e81812b494700f83c4a0cd3de0
LeoLoboGomes <leo_lobo_gomes@hotmail.com>
2020-10-22 03:23:16 -0300

    tentar ajeitar cors

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ app.use(function (req, res, next) {
   // }
   //
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   // res.header("Access-Control-Allow-Origin", "*");
   // //Quais são os métodos que a conexão pode realizar na API
   // res.setHeader("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,HEAD,OPTIONS');
