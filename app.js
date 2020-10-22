198122a37d595b55e11678c20caa59b8cbc62bf1
Rafaella Bambokian <rafaellabambokian@outlook.com>
2020-10-22 03:07:17 -0400

    try to solve cors

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ app.use(function (req, res, next) {
   // }
   //
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   // res.header("Access-Control-Allow-Origin", "*");
   // //Quais são os métodos que a conexão pode realizar na API
   // res.setHeader("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,HEAD,OPTIONS');
